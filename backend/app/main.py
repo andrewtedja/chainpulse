ac0c6af68ee766302ada0f661c25f17ae8fa3a81
Fozzzzy <andrewtedj@gmail.com>
2025-09-18 00:51:08 +0700

    feat: initialize next/fastapi app, dockerized

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from .database import get_db, Base, engine
+from . import models
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"text" : "Hello World"}
+
+# uvicorn app.main:app --reload
